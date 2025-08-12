eed8df28c57bb352d0ef9c7e2d8dcbfe43e14906
divyanshu <divanshus860@gmail.com>
2025-08-12 23:18:10 +0530

    halfway on the Express project Authorize Api where controllers like signup login are done with router setups,  now protecting routes with middleware in next step

---
+++ Express/auth-api/controllers/authController.js
@@ -0,0 +1,59 @@
+const bcrypt = require("bcrypt");
+const { response, json } = require("express");
+const jwt = require("jsonwebtoken")
+
+const users = [];
+
+const signup = async(req,res)=>{
+    const {username,password} = req.body;
+
+    if (!username || !password) {
+     return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    const cleanUsername = username.trim().toLowerCase();
+
+    const existingUser = users.find(
+        u => u.username === cleanUsername
+    );
+    if (existingUser) {
+        return res.status(400).json({ error: 'Username already taken' });
+    }
+
+    try{
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const newUser = {username:cleanUsername, password:hashedPassword, role:"user"};
+        users.push(newUser);
+        res.status(201).json({msg:"User created successfully"});
+    }
+    catch(err){
+        res.status(500).json({error:"Oops! something went wrong"});
+    }
+};
+
+const login=async(req,res)=>{
+    const{username,password}=req.body;
+    const cleanUsername = username.trim().toLowerCase();
+
+    const user = users.find(u=>u.username===cleanUsername);
+
+    if(!user){
+        return res.status(400).json({error:"User or password invalid"})
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return res.status(400).json({error:"User or password invalid"})
+    }
+
+    const token = jwt.sign(
+        {username:user.username,role:user.role},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.TOKEN_EXPIRY}
+    );
+
+    res.json({messg: "Login success",token});
+};
+
+module.exports={signup,login,users};
